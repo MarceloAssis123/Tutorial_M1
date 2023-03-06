a9a05a59b07beab468863c9324bb6c53de78d8fd
MarceloAssis123 <124265415+MarceloAssis123@users.noreply.github.com>
2023-03-06 05:39:43 -0300

    Código comentado

--- SEMANA_05/03_FEITO/Tarefa.gd
+++ SEMANA_05/03_FEITO/Tarefa.gd
@@ -8,9 +8,17 @@ var lista = []
 
 func _on_Button_pressed():
 	#Coletando dados inseridos pelo usuário
+	
+	#Nessa função eu mudei praticamente tudo, já que da forma que estava só se
+	#armazenava o número e o nome não tinha como ser inserido pelo usuario.
+	
+	#Nessas 3 primeiras linhas eu armazeno o número inserido pelo usuario em na
+	#variável numero e mostro na tela o número armazenado
 	numero = float($LineEdit.text)
 	$LineEdit.text = ""
 	$Label.text = "Número coletado: " + String(numero)
+	#Nessas outras 3 linhas eu armazeno o nome inserido pelo usuario na variável nome e
+	#mostro na Label o nome armazenado
 	nome = $LineEdit2.text
 	$LineEdit2.text = ""
 	$Label5.text = "Nome coletado: " + nome
@@ -20,6 +28,10 @@ func _on_Button_pressed():
 
 func _on_Button2_pressed():
 	#Incrementando o número inserido pelo usuário
+	
+	#Nessa função eu mudei o incremento no número para fazer sentido o botão 3, visto que
+	#da forma que estava antes era adicionado números ímpares ao número inserido pelo
+	#usuario resultando assim sempre em um algum número ímpar na lista 
 	lista = []
 	for i in range(10):
 		lista.append(numero)
@@ -30,6 +42,12 @@ func _on_Button2_pressed():
 func _on_Button3_pressed():
 	#Mudando o nome do usuário de acordo com os dados da lista
 	#Se houver algum número ímpar o nome deve adicionar "baldo" ao final
+	
+	#Nessa função eu mudei a forma identificar a presença de algum número ímpar na lista, visto que
+	#como está sendo incrementado números pares somente o primeiro número importa, já que se
+	#ele for par todos da lista também serão pares, e ainda acresentei uma Label que informa 
+	#o se o número foi alterado ou não.
+	
 	if int(numero) % 2 == 0:
 		$Label6.text = "Novo nome: " + nome
 		$Label7.text = "Nome não alterado"
