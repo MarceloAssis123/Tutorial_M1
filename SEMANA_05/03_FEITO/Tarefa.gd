54dcf923da0c70fdb69e568bdfeb6f18c7a72890
MarceloAssis123 <124265415+MarceloAssis123@users.noreply.github.com>
2023-03-06 05:42:59 -0300

    correção dos comentários

--- SEMANA_05/03_FEITO/Tarefa.gd
+++ SEMANA_05/03_FEITO/Tarefa.gd
@@ -10,7 +10,7 @@ func _on_Button_pressed():
 	#Coletando dados inseridos pelo usuário
 	
 	#Nessa função eu mudei praticamente tudo, já que da forma que estava só se
-	#armazenava o número e o nome não tinha como ser inserido pelo usuario.
+	#armazenava o número, e o nome não tinha como ser inserido pelo usuario.
 	
 	#Nessas 3 primeiras linhas eu armazeno o número inserido pelo usuario em na
 	#variável numero e mostro na tela o número armazenado
@@ -43,10 +43,10 @@ func _on_Button3_pressed():
 	#Mudando o nome do usuário de acordo com os dados da lista
 	#Se houver algum número ímpar o nome deve adicionar "baldo" ao final
 	
-	#Nessa função eu mudei a forma identificar a presença de algum número ímpar na lista, visto que
+	#Nessa função eu mudei a forma de identificar a presença de algum número ímpar na lista, visto que
 	#como está sendo incrementado números pares somente o primeiro número importa, já que se
 	#ele for par todos da lista também serão pares, e ainda acresentei uma Label que informa 
-	#o se o número foi alterado ou não.
+	#o se o nome foi alterado ou não.
 	
 	if int(numero) % 2 == 0:
 		$Label6.text = "Novo nome: " + nome
